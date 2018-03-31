4af894a0f7b4e254cd5da3479c4eba1b60f6ad4c
An TRUONG <vanan.sdsmnc.221@gmail.com>
2018-03-31 12:53:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const express  = require('express'),
     app.use(cors());
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+        res.header('Access-Control-Allow-Methods', '*');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
